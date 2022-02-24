aabb1742a0533d8b38764c0db959149ce64c011f
Viney <vineykhaneja999@gmail.com>
2022-02-24 11:44:26 +0530

    Add project files.

---
+++ LinkListMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List_Problem_Statements
+{
+    internal class LinkListMethods
+    {
+
+        public Node head;
+        //For inserting elements at last in LinkedList.
+        public void InsertElementsInLinkedList(int value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head = newNode;
+                Console.WriteLine($"{newNode.data} is being added.");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;    
+                }
+                temp.next = newNode;
+                Console.WriteLine($"{newNode.data} is being added.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linked_List_Problem_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkListMethods linkListMethods = new LinkListMethods();
+            linkListMethods.InsertElementsInLinkedList(12);
+        }
+    }
+}
