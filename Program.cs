92d98759c406228467bb294eb5802f3ed85d4ed0
Viney <vineykhaneja999@gmail.com>
2022-02-24 13:02:49 +0530

    [Viney] FEATURE ADDED: Apending elements to last

--- Program.cs
+++ Program.cs
@@ -10,11 +10,9 @@ namespace Linked_List_Problem_Statements
         static void Main(string[] args)
         {
             LinkListMethods linkListMethods = new LinkListMethods();
-            //Creating Node of 70.
+            linkListMethods.InsertElementsInLinkedList(56);
+            linkListMethods.InsertElementsInLinkedList(30);
             linkListMethods.InsertElementsInLinkedList(70);
-            //Adding elements to the node of 70.
-            linkListMethods.AddingElementsToNode(30);
-            linkListMethods.AddingElementsToNode(56);
             //Verifying if elements are added or not.
             linkListMethods.DisplayLinkedList();
         }
